b64df0e60e7b164dbafb44adc484335adf55a92c
Leonard Oduneye <chasisdude@gmail.com>
2022-09-09 14:25:26 +0100

    setting up db connection

--- src/configs/db/index.js
+++ src/configs/db/index.js
@@ -1,3 +1,27 @@
 const { Pool } = require('pg');
 require("dotenv").config();
 
+const productionENV = process.env.NODE_ENV === "production";
+
+const database = process.env.NODE_ENV === "dev" ? process.env.DB_DEV : process.env.DB_PROD
+
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
+
+const pool = new Pool({
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  /*
+    SSL is not supported in development
+    Alternatively, you can omit the ssl configuration object if you specify the PGSSLMODE config var: heroku config:set PGSSLMODE=no-verify
+    See https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+    */
+  ssl: isProduction //
+    ? { rejectUnauthorized: false }
+    : false,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
+};
